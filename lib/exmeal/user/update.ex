cc879b894151ebce92dc43178ce6e972866d5bd6
Jefferson Farias <jefferson.farias7@gmail.com>
2022-03-16 07:34:23 -0300

    update user

--- lib/exmeal/user/update.ex
+++ lib/exmeal/user/update.ex
@@ -1,7 +1,11 @@
 defmodule Exmeal.Users.Update do
-  alias Exmeal.{Error, Repo, User}
+  alias Exmeal.{Repo, User, Users}
+  alias Users.Get
 
   def call(%{"id" => id} = params) do
-    # TO DO
+    with {:ok, %User{} = user} <- Get.by_id(id) do
+      User.changeset(user, params)
+      |> Repo.update()
+    end
   end
 end
